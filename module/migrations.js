18619d58ed610452a702a49168b3e27393e594ad
Tom Rodriguez <tmrodrig@gmail.com>
2020-11-03 08:29:21 -0800

    Implement calculation of SkillBase

--- module/migrations.js
+++ module/migrations.js
@@ -50,14 +50,14 @@ export async function migrateWorld() {
 
     // Migrate World Compendium Packs
     // const packs = game.packs.filter(p => {
-    //     return (p.metadata.package === "world") && ["Actor", "Item", "Scene"].includes(p.metadata.entity);
+    //     return (p.metadata.package === 'world') && ['Actor', 'Item', 'Scene'].includes(p.metadata.entity);
     // });
     // for (let p of packs) {
     //     await migrateCompendium(p);
     // }
 
     // Set migration as complete
-    game.settings.set("hm3", "systemMigrationVersion", game.system.data.version);
+    game.settings.set('hm3', 'systemMigrationVersion', game.system.data.version);
     console.log(`HM3 | Completed migration to version ${game.system.data.version}`);
     ui.notifications.info(`HM3 System Migration to version ${game.system.data.version} completed!`, {permanent: true});  
 };
@@ -71,40 +71,57 @@ export async function migrateActorData(actor) {
     // numbers to objects containing base and effective
     // Guard against re-implementing the upgrade here
     if (typeof actorData.data.abilities.strength != 'object') {
-        updateData["data.abilities.strength.base"] = actorData.data.abilities.strength;
-        updateData["data.abilities.stamina.base"] = actorData.data.abilities.stamina;
-        updateData["data.abilities.dexterity.base"] = actorData.data.abilities.dexterity;
-        updateData["data.abilities.agility.base"] = actorData.data.abilities.agility;
-        updateData["data.abilities.intelligence.base"] = actorData.data.abilities.intelligence;
-        updateData["data.abilities.aura.base"] = actorData.data.abilities.aura;
-        updateData["data.abilities.will.base"] = actorData.data.abilities.will;
-        updateData["data.abilities.eyesight.base"] = actorData.data.abilities.eyesight;
-        updateData["data.abilities.hearing.base"] = actorData.data.abilities.hearing;
-        updateData["data.abilities.smell.base"] = actorData.data.abilities.smell;
-        updateData["data.abilities.voice.base"] = actorData.data.abilities.voice;
-        updateData["data.abilities.comliness.base"] = actorData.data.abilities.comliness;
-        updateData["data.abilities.morality.base"] = actorData.data.abilities.morality;
+        updateData['data.abilities.strength.base'] = actorData.data.abilities.strength;
+        updateData['data.abilities.stamina.base'] = actorData.data.abilities.stamina;
+        updateData['data.abilities.dexterity.base'] = actorData.data.abilities.dexterity;
+        updateData['data.abilities.agility.base'] = actorData.data.abilities.agility;
+        updateData['data.abilities.intelligence.base'] = actorData.data.abilities.intelligence;
+        updateData['data.abilities.aura.base'] = actorData.data.abilities.aura;
+        updateData['data.abilities.will.base'] = actorData.data.abilities.will;
+        updateData['data.abilities.eyesight.base'] = actorData.data.abilities.eyesight;
+        updateData['data.abilities.hearing.base'] = actorData.data.abilities.hearing;
+        updateData['data.abilities.smell.base'] = actorData.data.abilities.smell;
+        updateData['data.abilities.voice.base'] = actorData.data.abilities.voice;
+        updateData['data.abilities.comliness.base'] = actorData.data.abilities.comliness;
+        updateData['data.abilities.morality.base'] = actorData.data.abilities.morality;
 
-        updateData["data.abilities.strength.effective"] = 0;
-        updateData["data.abilities.stamina.effective"] = 0;
-        updateData["data.abilities.dexterity.effective"] = 0;
-        updateData["data.abilities.agility.effective"] = 0;
-        updateData["data.abilities.intelligence.effective"] = 0;
-        updateData["data.abilities.aura.effective"] = 0;
-        updateData["data.abilities.will.effective"] = 0;
-        updateData["data.abilities.eyesight.effective"] = 0;
-        updateData["data.abilities.hearing.effective"] = 0;
-        updateData["data.abilities.smell.effective"] = 0;
-        updateData["data.abilities.voice.effective"] = 0;
-        updateData["data.abilities.comliness.effective"] = 0;
-        updateData["data.abilities.morality.effective"] = 0;
+        updateData['data.abilities.strength.effective'] = 0;
+        updateData['data.abilities.stamina.effective'] = 0;
+        updateData['data.abilities.dexterity.effective'] = 0;
+        updateData['data.abilities.agility.effective'] = 0;
+        updateData['data.abilities.intelligence.effective'] = 0;
+        updateData['data.abilities.aura.effective'] = 0;
+        updateData['data.abilities.will.effective'] = 0;
+        updateData['data.abilities.eyesight.effective'] = 0;
+        updateData['data.abilities.hearing.effective'] = 0;
+        updateData['data.abilities.smell.effective'] = 0;
+        updateData['data.abilities.voice.effective'] = 0;
+        updateData['data.abilities.comliness.effective'] = 0;
+        updateData['data.abilities.morality.effective'] = 0;
     }
+
+    if (typeof actorData.data.sunsign === 'undefined') {
+        updateData['data.sunsign'] = '';
+    }
+
+    if (typeof actorData.data.species === 'undefined') {
+        updateData['data.species'] = '';
+    }
+
+    if (typeof actorData.data.biography === 'undefined') {
+        updateData['data.biography'] = '';
+    }
+
     // process items
     for (let i of actor.items) {
         const migrateData = migrateItemData(i.data);
         if (migrateData != null) {
-            console.log(`HM3 | Migrating Actor ${actorData.name}, Item ${i.data.name}`);
-            await i.update(migrateData);
+            console.log(`HM3 | Migrated Actor ${actor.data.name}, Item ${i.data.name}`);
+            try {
+                await i.update(migrateData);
+            } catch (err) {
+                console.error(err);
+            }
         }
     }
 
@@ -113,30 +130,42 @@ export async function migrateActorData(actor) {
 
 export function migrateItemData(itemData) {
     const data = itemData.data;
+    let isModified = false;
 
     // migrate if needed
     if (itemData.type.endsWith('gear')) {
         if (typeof data.isEquipped === 'undefined') {
             data.isEquipped = false;
             data.isCarried = true;
+            isModified = true;
         }
     
         if (itemData.type === 'armorlocation') {
             if (typeof data.protection === 'undefined') {
-            data.protection = {
-                "blunt": 0,
-                "edged": 0,
-                "piercing": 0,
-                "fire": 0
-            };
-            data.locations = [];
+                data.protection = {
+                    'blunt': 0,
+                    'edged': 0,
+                    'piercing': 0,
+                    'fire': 0
+                };
+                data.locations = [];
+                isModified = true;
             }
         }
-
-        return itemData;
     }
 
-    return null;
+    if (itemData.type.endsWith('skill') || itemData.type === 'psionic') {
+        if (typeof data.skillBase != 'object') {
+            const value = data.skillBase;
+            data.skillBase = {};
+            data.skillBase.value = value;
+            data.skillBase.formula = '';
+            data.skillBase.isFormulaValid = true;
+            isModified = true;
+        }
+    }
+
+    return isModified ? itemData : null;
 }
 
 export function migrateSceneData(scene) {
---
+++ module/utility.js
@@ -0,0 +1,195 @@
+/**
+ * Determines whether the Skill Base Formula is valid. We perform that
+ * validation here so even a skill not associated with a particular
+ * actor can have its formula validated.
+ * 
+ * A valid SB formula looks like this:
+ * 
+ *   "@str, @int, @sta, hirin:2, ahnu, 5"
+ * 
+ * meaning
+ *   average STR, INT, and STA
+ *   add 2 if sunsign hirin (modifier after colon ":")
+ *   add 1 if sunsign ahnu (1 since no modifier specified)
+ *   add 5 to result
+ * 
+ * A valid formula must have exactly 3 abilities, everything else is optional.
+ * 
+ * The result of this function is to set the "isFormulaValid" value appropriately.
+ * 
+ * @param {Object} item
+ */
+export function calcSkillBase(item) {
+    const sb = item.data.data.skillBase;
+
+    let actorData = null;
+    if (item.actor) {
+        actorData = item.actor.data.data;
+    }
+
+    sb.isFormulaValid = true;
+    if (sb.formula === '') {
+        // If the formula is blank, its valid,
+        // don't touch the existing value.
+        return;
+    }
+
+    let numAbilities = 0;
+    let sumAbilities = 0;
+    let modifier = 0;
+    let resultSB = 0;
+
+    // All parts of the formula are separated by commas,
+    // and we lowercase here since the string is processed
+    // case-insensitive.
+    const sbParts = sb.formula.toLowerCase().split(',');
+
+    // Formula must have at least three abilities, and therefore
+    // we must have at least three parts, otherwise it is invalid
+    if (sbParts.length < 3) {
+        sb.isFormulaValid = false;
+        return;
+    }
+
+    sbParts.forEach(param => {
+        if (!sb.isFormulaValid) return;
+
+        param = param.trim();
+        if (param != '') {
+            if (param.startsWith('@')) {
+                // This is a reference to an ability
+
+                // Must have more than just the '@' sign
+                if (param.length === 1) {
+                    sb.isFormulaValid = false;
+                    return;
+                }
+
+                // There may only be 3 abilities
+                if (numAbilities >= 3) {
+                    sb.isFormulaValid = false;
+                    return;
+                }
+
+                if (actorData) {
+                    const paramName = param.slice(1);
+                    switch (paramName) {
+                        case 'str':
+                            sumAbilities += actorData.abilities.strength.base;
+                            break;
+
+                        case 'sta':
+                            sumAbilities += actorData.abilities.stamina.base;
+                            break;
+
+                        case 'dex':
+                            sumAbilities += actorData.abilities.dexterity.base;
+                            break;
+
+                        case 'agl':
+                            sumAbilities += actorData.abilities.agility.base;
+                            break;
+
+                        case 'int':
+                            sumAbilities += actorData.abilities.intelligence.base;
+                            break;
+
+                        case 'aur':
+                            sumAbilities += actorData.abilities.aura.base;
+                            break;
+
+                        case 'wil':
+                            sumAbilities += actorData.abilities.will.base;
+                            break;
+
+                        case 'eye':
+                            sumAbilities += actorData.abilities.eyesight.base;
+                            break;
+
+                        case 'hrg':
+                            sumAbilities += actorData.abilities.hearing.base;
+                            break;
+
+                        case 'sml':
+                            sumAbilities += actorData.abilities.smell.base;
+                            break;
+
+                        case 'voi':
+                            sumAbilities += actorData.abilities.voice.base;
+                            break;
+
+                        case 'com':
+                            sumAbilities += actorData.abilities.comliness.base;
+                            break;
+
+                        case 'mor':
+                            sumAbilities += actorData.abilities.morality.base;
+                            break;
+    
+                        default:
+                            sb.isFormulaValid = false;
+                            return;
+                    }
+                }
+
+                numAbilities++;
+                return;
+            }
+
+            if (param.match(/^[a-z]/)) {
+                // This is a sunsign
+
+                let ssParts = param.split(':');
+
+                // if more than 2 parts, it's invalid
+                if (ssParts.length > 2) {
+                    sb.isFormulaValid = false;
+                    return;
+                }
+
+                // if second part provided, must be a number
+                if (ssParts.length === 2 && !ssParts[1].trim().match(/[-+]?\d+/)) {
+                    sb.isFormulaValid = false;
+                    return;
+                }
+
+                if (actorData) {
+                    // we must get the actor's sunsign to see if it matches. Actors may
+                    // specify the sunsign as a dual sunsign, in which case the two parts
+                    // must be separated either by a dash or a forward slash
+                    let actorSS = actorData.sunsign.trim().toLowerCase().split(/[-\/]/);
+                    
+                    // Call 'trim' function on all strings in actorSS
+                    actorSS.map(Function.prototype.call, String.prototype.trim);
+
+                    // Now, check whether our sunsign matches any of the actor's sunsigns
+                    if (actorSS.includes(ssParts[0])) {
+                        // We matched a character's sunsign, apply modifier
+
+                        modifier += ssParts.length === 2 ? Number(ssParts[1].trim()) : 1;
+                    }
+                }
+
+                return;
+            }
+
+            // The only valid possibility left is a number.
+            // If it's not a number, it's invalid.
+            if (param.match(/^[-+]?\d+$/)) {
+                modifier += Number(param);
+            } else {
+                sb.isFormulaValid = false;
+                return;
+            }
+        }
+    });
+
+    if (numAbilities != 3) {
+        sb.isFormulaValid = false;
+    } else {
+        if (actorData) {
+            sb.value = Math.round((sumAbilities / 3) + Number.EPSILON) + modifier;
+        }
+    }
+}
+
