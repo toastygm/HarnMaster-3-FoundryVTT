0490d24e086553f664369abe4dad1bab32630f20
Tom Rodriguez <tmrodrig@gmail.com>
2020-11-20 07:51:52 -0800

    add default image when creating actor items

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -63,25 +63,25 @@ export class HarnMasterActor extends Actor {
     const commSkill = mergeObject({type: "Communication"}, game.system.model.Item.skill, {overwrite:false});
     const combatSkill = mergeObject({type: "Combat"}, game.system.model.Item.skill, {overwrite:false});
     const craftSkill = mergeObject({type: "Craft"}, game.system.model.Item.skill, {overwrite:false});
-    data.items.push((new Item({name: 'Climbing', type: 'skill', data: physicalSkill})).data);
-    data.items.push((new Item({name: 'Condition', type: 'skill', data: physicalSkill})).data);
-    data.items.push((new Item({name: 'Jumping', type: 'skill', data: physicalSkill})).data);
-    data.items.push((new Item({name: 'Stealth', type: 'skill', data: physicalSkill})).data);
-    data.items.push((new Item({name: 'Throwing', type: 'skill', data: physicalSkill})).data);
-    data.items.push((new Item({name: 'Awareness', type: 'skill', data: commSkill})).data);
-    data.items.push((new Item({name: 'Intrigue', type: 'skill', data: commSkill})).data);
-    data.items.push((new Item({name: 'Oratory', type: 'skill', data: commSkill})).data);
-    data.items.push((new Item({name: 'Rhetoric', type: 'skill', data: commSkill})).data);
-    data.items.push((new Item({name: 'Singing', type: 'skill', data: commSkill})).data);
-    data.items.push((new Item({name: 'Initiative', type: 'skill', data: combatSkill})).data);
-    data.items.push((new Item({name: 'Unarmed', type: 'skill', data: combatSkill})).data);
-    data.items.push((new Item({name: 'Dodge', type: 'skill', data: combatSkill})).data);
+    data.items.push((new Item({name: 'Climbing', type: 'skill', data: physicalSkill, img: 'systems/hm3/images/icons/svg/climbing.svg'})).data);
+    data.items.push((new Item({name: 'Condition', type: 'skill', data: physicalSkill, img: 'systems/hm3/images/icons/svg/muscle.svg'})).data);
+    data.items.push((new Item({name: 'Jumping', type: 'skill', data: physicalSkill, img: 'systems/hm3/images/icons/svg/jump.svg'})).data);
+    data.items.push((new Item({name: 'Stealth', type: 'skill', data: physicalSkill, img: 'systems/hm3/images/icons/svg/stealth.svg'})).data);
+    data.items.push((new Item({name: 'Throwing', type: 'skill', data: physicalSkill, img: 'systems/hm3/images/icons/svg/throw.svg'})).data);
+    data.items.push((new Item({name: 'Awareness', type: 'skill', data: commSkill, img: 'systems/hm3/images/icons/svg/awareness.svg'})).data);
+    data.items.push((new Item({name: 'Intrigue', type: 'skill', data: commSkill, img: 'systems/hm3/images/icons/png/cloak-dagger.png'})).data);
+    data.items.push((new Item({name: 'Oratory', type: 'skill', data: commSkill, img: 'systems/hm3/images/icons/svg/speaking.svg'})).data);
+    data.items.push((new Item({name: 'Rhetoric', type: 'skill', data: commSkill, img: 'systems/hm3/images/icons/svg/rhetoric.svg'})).data);
+    data.items.push((new Item({name: 'Singing', type: 'skill', data: commSkill, img: 'systems/hm3/images/icons/svg/musician-singing.svg'})).data);
+    data.items.push((new Item({name: 'Initiative', type: 'skill', data: combatSkill, img: 'systems/hm3/images/icons/svg/initiative.svg'})).data);
+    data.items.push((new Item({name: 'Unarmed', type: 'skill', data: combatSkill, img: 'systems/hm3/images/icons/svg/punch.svg'})).data);
+    data.items.push((new Item({name: 'Dodge', type: 'skill', data: combatSkill, img: 'systems/hm3/images/icons/svg/dodge.svg'})).data);
   }
 
   static _createDefaultCreatureSkills(data) {
     const combatSkill = mergeObject({type: "Combat"}, game.system.model.Item.skill, {overwrite:false});
-    data.items.push((new Item({name: 'Initiative', type: 'skill', data: combatSkill})).data);
-    data.items.push((new Item({name: 'Dodge', type: 'skill', data: combatSkill})).data);
+    data.items.push((new Item({name: 'Initiative', type: 'skill', data: combatSkill, img: 'systems/hm3/images/icons/svg/initiative.svg'})).data);
+    data.items.push((new Item({name: 'Dodge', type: 'skill', data: combatSkill, img: 'systems/hm3/images/icons/svg/dodge.svg'})).data);
   }
 
   static _createDefaultHumanoidLocations(data) {
