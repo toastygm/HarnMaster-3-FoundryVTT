1cd3040cff8a742ad744feabaf47aef786d860b0
Tom Rodriguez <tmrodrig@gmail.com>
2020-11-24 14:28:12 -0800

    Support macros for rollable items

---
+++ module/macros.js
@@ -0,0 +1,489 @@
+import { DiceHM3 } from './dice-hm3.js';
+
+/**
+ * Create a script macro from an Item drop.
+ * Get an existing item macro if one exists, otherwise create a new one.
+ * @param {Object} data     The dropped data
+ * @param {number} slot     The hotbar slot to use
+ * @returns {Promise}
+ */
+export async function createHM3Macro(data, slot) {
+    if (data.type !== "Item") return;
+    if (!data.data) return ui.notifications.warn("No macro exists for that type of object.");
+    const item = data.data;
+
+    let command;
+    switch (item.type) {
+        case 'skill':
+            if (item.data.type !== 'Psionic') {
+                command = `game.hm3.macros.skillRoll("${item.name}");`;
+            } else {
+                command = `game.hm3.macros.usePsionicRoll("${item.name}");`;
+            }
+            break;
+
+        case 'spell':
+            command = `game.hm3.macros.castSpellRoll("${itemName}");`;
+            break;
+
+        case 'invocation':
+            command = `game.hm3.macros.invokeRitualRoll("${itemName}");`;
+            break;
+
+        case 'weapongear':
+            return await askWeaponMacro(item.name, slot, item.img);
+            command = `game.hm3.macros.weaponAttackRoll("${itemName}");`;
+            break;
+
+        case 'misslegear':
+            return await askMissileMacro(item.name, slot, item.img);
+            command = `game.hm3.macros.missileAttackRoll("${itemName}");`;
+            break;
+
+        case 'injury':
+            command = `game.hm3.macros.healingRoll("${itemName}");`;
+            break;
+
+        default:
+            return false;
+    }
+
+    return await applyMacro(item.name, command, slot, item.img, {"hm3.itemMacro": false});
+}
+
+async function applyMacro(name, command, slot, img, flags) {
+    let macro = game.macros.entities.find(m => (m.name === name) && (m.command === command));
+    if (!macro) {
+        macro = await Macro.create({
+            name: name,
+            type: "script",
+            img: img,
+            command: command,
+            flags: flags
+        });
+    }
+    game.user.assignHotbarMacro(macro, slot);
+    return false;
+}
+
+function askWeaponMacro(name, slot, img) {
+    const html = '<p>Select the type of weapon macro to create:</p>'
+    
+    // Create the dialog window
+    return new Promise(resolve => {
+        new Dialog({
+            title: 'Select Weapon Macro',
+            content: html.trim(),
+            buttons: {
+                attackButton: {
+                    label: "Attack",
+                    callback: async (html) => {
+                        return await applyMacro(name, `game.hm3.macros.weaponAttackRoll("${name}");`, slot, img, {"hm3.itemMacro": false});
+                    }
+                },
+                defendButton: {
+                    label: "Defend",
+                    callback: async (html) => {
+                        return await applyMacro(name, `game.hm3.macros.weaponDefendRoll("${name}");`, slot, img, {"hm3.itemMacro": false});
+                    }
+                },
+                damageButton: {
+                    label: "Damage",
+                    callback: async (html) => {
+                        return await applyMacro(name, `game.hm3.macros.weaponDamageRoll("${name}");`, slot, img, {"hm3.itemMacro": false});
+                    }
+                }
+            },
+            default: "attackButton",
+            close: () => resolve(false)
+        }).render(true)
+    });
+}
+
+function askMissileMacro(name, img) {
+    const html = '<p>Select the type of missile macro to create:</p>'
+    
+    // Create the dialog window
+    return new Promise(resolve => {
+        new Dialog({
+            title: 'Select Missile Macro',
+            content: html.trim(),
+            buttons: {
+                attackButton: {
+                    label: "Attack",
+                    callback: async (html) => {
+                        return await applyMacro(name, `game.hm3.macros.missileAttackRoll("${name}");`, slot, img, {"hm3.itemMacro": false});
+                    }
+                },
+                damageButton: {
+                    label: "Damage",
+                    callback: async (html) => {
+                        return await applyMacro(name, `game.hm3.macros.missileDamageRoll("${name}");`, slot, img, {"hm3.itemMacro": false});
+                    }
+                }
+            },
+            default: "attackButton",
+            close: () => resolve(false)
+        }).render(true)
+    });
+}
+
+export function skillRoll(itemName, noDialog = false) {
+    const speaker = ChatMessage.getSpeaker();
+    const lcItemName = itemName.toLowerCase();
+    const items = actor ? actor.items.filter(i => i.type === 'skill' && i.data.type !== 'Psionic' && i.name.toLowerCase() === lcItemName) : [];
+    if (items.length > 1) {
+        ui.notifications.warn(`Your controlled Actor ${actor.name} has more than one skill with name ${itemName}. The first matched skill will be chosen.`);
+    } else if (items.length === 0) {
+        ui.notifications.warn(`Your controlled Actor does not have a skill named ${itemName}`);
+        return null;
+    }
+    const item = items[0];
+    if (!item) return;
+
+    let actor;
+    if (speaker.token) actor = game.actors.tokens[speaker.token];
+    if (!actor) actor = game.actors.get(speaker.actor);
+    if (!actor) {
+        ui.notifications.warn(`No actor selected, roll ignored.`);
+        return;
+    }
+
+    const label = `${item.data.name} Skill Test`;
+    return actor._d100StdRoll(label, item.data.data.effectiveMasteryLevel, speaker, noDialog);
+}
+
+export function castSpellRoll(itemName, noDialog = false) {
+    const speaker = ChatMessage.getSpeaker();
+    let actor;
+    if (speaker.token) actor = game.actors.tokens[speaker.token];
+    if (!actor) actor = game.actors.get(speaker.actor);
+    if (!actor) {
+        ui.notifications.warn(`No actor selected, roll ignored.`);
+        return;
+    }
+
+    const item = getItem(itemName, 'spell', actor);
+    if (!item) return;
+
+    const label = `Casting ${item.data.name}`;
+    return actor._d100StdRoll(label, item.data.data.effectiveMasteryLevel, speaker, noDialog);
+}
+
+export function invokeRitualRoll(itemName, noDialog = false) {
+    const speaker = ChatMessage.getSpeaker();
+    let actor;
+    if (speaker.token) actor = game.actors.tokens[speaker.token];
+    if (!actor) actor = game.actors.get(speaker.actor);
+    if (!actor) {
+        ui.notifications.warn(`No actor selected, roll ignored.`);
+        return;
+    }
+
+    const item = getItem(itemName, 'invocation', actor);
+    if (!item) return;
+
+    const label = `Invoking ${item.data.name} Ritual`;
+    return actor._d100StdRoll(label, item.data.data.effectiveMasteryLevel, speaker, noDialog);
+}
+
+export function usePsionicRoll(itemName, noDialog = false) {
+    const speaker = ChatMessage.getSpeaker();
+    const lcItemName = itemName.toLowerCase();
+    let actor;
+    if (speaker.token) actor = game.actors.tokens[speaker.token];
+    if (!actor) actor = game.actors.get(speaker.actor);
+    if (!actor) {
+        ui.notifications.warn(`No actor selected, roll ignored.`);
+        return;
+    }
+
+    const items = actor ? actor.items.filter(i => i.type === 'skill' && i.data.type === 'Psionic' && i.name.toLowerCase() === lcItemName) : [];
+    if (items.length > 1) {
+        ui.notifications.warn(`Your controlled Actor ${actor.name} has more than one psionic with name ${itemName}. The first matched psionic will be chosen.`);
+    } else if (items.length === 0) {
+        ui.notifications.warn(`Your controlled Actor does not have a psionic named ${itemName}`);
+        return null;
+    }
+    const item = items[0];
+    if (!item) return;
+
+    const label = `Using ${item.data.name} Talent`;
+    return actor._d100StdRoll(label, item.data.data.effectiveMasteryLevel, speaker, noDialog);
+}
+
+export function testAbilityD6Roll(ability, noDialog = false) {
+    const speaker = ChatMessage.getSpeaker();
+    let actor;
+    if (speaker.token) actor = game.actors.tokens[speaker.token];
+    if (!actor) actor = game.actors.get(speaker.actor);
+    if (!actor) {
+        ui.notifications.warn(`No actor selected, roll ignored.`);
+        return;
+    }
+
+    let abilities;
+    if (actor.data.type === 'character') {
+        abilities = Object.keys(game.system.model.Actor.character.abilities);
+    } else if (actor.data.type === 'creature') {
+        abilities = Object.keys(game.system.model.Actor.creature.abilities);
+    } else {
+        return;
+    }
+    if (!ability || !abilities.contains(ability)) return;
+
+    const label = `d6 ${ability[0].toUpperCase()}${ability.slice(1)} Roll`;
+    return this._d6StdRoll(label, this.data.data.abilities[ability].effective, 3, speaker, noDialog);
+}
+
+export function testAbilityD100Roll(ability, noDialog = false) {
+    const speaker = ChatMessage.getSpeaker();
+    let actor;
+    if (speaker.token) actor = game.actors.tokens[speaker.token];
+    if (!actor) actor = game.actors.get(speaker.actor);
+    if (!actor) {
+        ui.notifications.warn(`No actor selected, roll ignored.`);
+        return;
+    }
+
+    let abilities;
+    if (actor.data.type === 'character') {
+        abilities = Object.keys(game.system.model.Actor.character.abilities);
+    } else if (actor.data.type === 'creature') {
+        abilities = Object.keys(game.system.model.Actor.creature.abilities);
+    } else {
+        return;
+    }
+    if (!ability || !abilities.contains(ability)) return;
+
+    const label = `d100 ${ability[0].toUpperCase()}${ability.slice(1)} Roll`;
+    return actor._d100StdRoll(label, Math.max(95, Math.min(5, item.data.data.abilities[ability].effective * 5)), speaker, noDialog);
+}
+
+export function weaponDamageRoll(itemName) {
+    const speaker = ChatMessage.getSpeaker();
+    let actor;
+    if (speaker.token) actor = game.actors.tokens[speaker.token];
+    if (!actor) actor = game.actors.get(speaker.actor);
+    if (!actor) {
+        ui.notifications.warn(`No actor selected, roll ignored.`);
+        return;
+    }
+
+    const item = getItem(itemName, 'weapongear', actor);
+    if (!item) return;
+
+    const rollData = {
+        weapon: item.data.name,
+        data: actor.data,
+        speaker: speaker
+    };
+    return DiceHM3.damageRoll(rollData);
+}
+
+export function missileDamageRoll(itemName) {
+    const speaker = ChatMessage.getSpeaker();
+    let actor;
+    if (speaker.token) actor = game.actors.tokens[speaker.token];
+    if (!actor) actor = game.actors.get(speaker.actor);
+    if (!actor) {
+        ui.notifications.warn(`No actor selected, roll ignored.`);
+        return;
+    }
+
+    const item = getItem(itemName, 'missilegear', actor);
+    if (!item) return;
+
+    const rollData = {
+        name: item.data.name,
+        aspect: item.data.data.weaponAspect,
+        impactShort: item.data.data.impact.short,
+        impactMedium: item.data.data.impact.medium,
+        impactLong: item.data.data.impact.long,
+        impactExtreme: item.data.data.impact.extreme,
+        data: actor.data,
+        speaker: speaker
+    };
+    return DiceHM3.missileDamageRoll(rollData);
+}
+
+export function weaponAttackRoll(itemName, noDialog = false) {
+    const speaker = ChatMessage.getSpeaker();
+    let actor;
+    if (speaker.token) actor = game.actors.tokens[speaker.token];
+    if (!actor) actor = game.actors.get(speaker.actor);
+    if (!actor) {
+        ui.notifications.warn(`No actor selected, roll ignored.`);
+        return;
+    }
+
+    const item = getItem(itemName, 'weapongear', actor);
+    if (!item) return;
+
+    const label = `${item.data.name} Attack`;
+    const rollData = {
+        label: label,
+        target: item.data.data.attackMasteryLevel,
+        fastforward: noDialog,
+        data: this.data,
+        speaker: speaker
+    };
+    return DiceHM3.d100StdRoll(rollData);
+}
+
+export function weaponDefendRoll(itemName, noDialog = false) {
+    const speaker = ChatMessage.getSpeaker();
+    let actor;
+    if (speaker.token) actor = game.actors.tokens[speaker.token];
+    if (!actor) actor = game.actors.get(speaker.actor);
+    if (!actor) {
+        ui.notifications.warn(`No actor selected, roll ignored.`);
+        return;
+    }
+
+    const item = getItem(itemName, 'weapongear', actor);
+    if (!item) return;
+
+    const label = `${item.data.name} Defense`;
+    const rollData = {
+        label: label,
+        target: item.data.data.defenseMasteryLevel,
+        fastforward: noDialog,
+        data: this.data,
+        speaker: speaker
+    };
+    return DiceHM3.d100StdRoll(rollData);
+}
+
+export function missileAttackRoll(itemName) {
+    const speaker = ChatMessage.getSpeaker();
+    let actor;
+    if (speaker.token) actor = game.actors.tokens[speaker.token];
+    if (!actor) actor = game.actors.get(speaker.actor);
+    if (!actor) {
+        ui.notifications.warn(`No actor selected, roll ignored.`);
+        return;
+    }
+
+    const item = getItem(itemName, 'missilegear', actor);
+    if (!item) return;
+
+    const rollData = {
+        name: item.data.name,
+        target: item.data.data.attackMasteryLevel,
+        aspect: item.data.data.defaultAspect,
+        rangeShort: item.data.data.range.short,
+        rangeMedium: item.data.data.range.medium,
+        rangeLong: item.data.data.range.long,
+        rangeExtreme: item.data.data.range.extreme,
+        data: this.data,
+        speaker: speaker
+    }
+    return DiceHM3.missileAttackRoll(rollData);
+}
+
+export function injuryRoll() {
+    const speaker = ChatMessage.getSpeaker();
+    let actor;
+    if (speaker.token) actor = game.actors.tokens[speaker.token];
+    if (!actor) actor = game.actors.get(speaker.actor);
+    if (!actor) {
+        ui.notifications.warn(`No actor selected, roll ignored.`);
+        return;
+    }
+
+    const rollData = {
+        actor: this,
+        speaker: speaker
+    };
+    return DiceHM3.injuryRoll(rollData);
+}
+
+export function healingRoll(itemName, noDialog = false) {
+    const speaker = ChatMessage.getSpeaker();
+    let actor;
+    if (speaker.token) actor = game.actors.tokens[speaker.token];
+    if (!actor) actor = game.actors.get(speaker.actor);
+    if (!actor) {
+        ui.notifications.warn(`No actor selected, roll ignored.`);
+        return;
+    }
+
+    const item = getItem(itemName, 'missilegear', actor);
+    if (!item) return;
+
+    const label = `${item.data.name} Healing Roll`;
+    return actor._d100StdRoll(label, item.data.data.healRate, speaker, noDialog);
+}
+
+export function shockRoll(noDialog = false) {
+    const speaker = ChatMessage.getSpeaker();
+    let actor;
+    if (speaker.token) actor = game.actors.tokens[speaker.token];
+    if (!actor) actor = game.actors.get(speaker.actor);
+    if (!actor) {
+        ui.notifications.warn(`No actor selected, roll ignored.`);
+        return;
+    }
+
+    const label = `Shock Roll`;
+    return actor._d6StdRoll(label, actor.data.data.endurance.value, actor.data.data.universalPenalty, speaker, noDialog);
+}
+
+export function stumbleRoll(noDialog = false) {
+    const speaker = ChatMessage.getSpeaker();
+    let actor;
+    if (speaker.token) actor = game.actors.tokens[speaker.token];
+    if (!actor) actor = game.actors.get(speaker.actor);
+    if (!actor) {
+        ui.notifications.warn(`No actor selected, roll ignored.`);
+        return;
+    }
+
+    const label = `Stumble Roll`;
+    return actor._d6StdRoll(label, actor.data.data.stumbleTarget, 3, speaker, noDialog);
+}
+
+export function fumbleRoll(noDialog = false) {
+    const speaker = ChatMessage.getSpeaker();
+    let actor;
+    if (speaker.token) actor = game.actors.tokens[speaker.token];
+    if (!actor) actor = game.actors.get(speaker.actor);
+    if (!actor) {
+        ui.notifications.warn(`No actor selected, roll ignored.`);
+        return;
+    }
+
+    const label = `Fumble Roll`;
+    return actor._d6StdRoll(label, actor.data.data.fumbleTarget, 3, speaker, noDialog);
+}
+
+export function genericDamageRoll() {
+    const speaker = ChatMessage.getSpeaker();
+    let actor;
+    if (speaker.token) actor = game.actors.tokens[speaker.token];
+    if (!actor) actor = game.actors.get(speaker.actor);
+    if (!actor) {
+        ui.notifications.warn(`No actor selected, roll ignored.`);
+        return;
+    }
+
+    const rollData = {
+        weapon: '',
+        data: actor.data,
+        speaker: speaker
+    };
+    return DiceHM3.damageRoll(rollData);
+}
+
+function getItem(itemName, type, actor) {
+    const lcItemName = itemName.toLowerCase();
+    const items = actor ? actor.items.filter(i => i.type === type && i.name.toLowerCase() === lcItemName) : [];
+    if (items.length > 1) {
+        ui.notifications.warn(`Your controlled Actor ${actor.name} has more than one ${type} with name ${itemName}. The first matched ${type} will be chosen.`);
+    } else if (items.length === 0) {
+        ui.notifications.warn(`Your controlled Actor does not have a ${type} named ${itemName}`);
+        return null;
+    }
+    return items[0];
+}
