566e31f7bcbd847260c3c4a64e192abe08458b01
Tom Rodriguez <tmrodrig@gmail.com>
2020-12-17 11:34:19 -0800

    increasing window width to allow new tab

--- module/actor/creature-sheet.js
+++ module/actor/creature-sheet.js
@@ -10,7 +10,7 @@ export class HarnMasterCreatureSheet extends HarnMasterBaseActorSheet {
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
       classes: ["hm3", "sheet", "actor", "creature"],
-      width: 660,
+      width: 700,
       height: 640,
       tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "facade" }]
     });
