ea78e89b7cce17f5384f37c432cbfc7f2ec71de9
Tom Rodriguez <tmrodrig@gmail.com>
2020-12-19 14:58:33 -0800

    Add dialog to get effect start time

--- module/effect.js
+++ module/effect.js
@@ -5,23 +5,61 @@
  * @param {MouseEvent} event      The left-click event on the effect control
  * @param {Actor|Item} owner      The owning entity which manages this effect
  */
-export function onManageActiveEffect(event, owner) {
+export async function onManageActiveEffect(event, owner) {
     event.preventDefault();
     const a = event.currentTarget;
     const li = a.closest("li");
     const effect = li.dataset.effectId ? owner.effects.get(li.dataset.effectId) : null;
     switch (a.dataset.action) {
         case "create":
-            return ActiveEffect.create({
-                label: "New Effect",
-                icon: "icons/svg/aura.svg",
-                origin: owner.uuid,
-                "duration.rounds": li.dataset.effectType === "temporary" ? 1 : undefined,
-                disabled: li.dataset.effectType === "inactive"
-            }, owner).create();
+            const dlgTemplate = "systems/hm3/templates/dialog/active-effect-start.html";
+            const dialogData = {
+                gameTime: game.time.worldTime
+            };
+            if (game.combat) {
+                dialogData.combatId = game.combat.id;
+                dialogData.combatRound = game.combat.round;
+                dialogData.combatTurn = game.combat.turn;
+            }
+            const html = await renderTemplate(dlgTemplate, dialogData);
+    
+            // Create the dialog window
+            return Dialog.prompt({
+                title: "Select Start Time",
+                content: html,
+                label: "OK",
+                callback: async (html) => {
+                    const form = html.querySelector('#active-effect-start');
+                    const fd = new FormDataExtended(form);
+                    const formdata = fd.toObject();
+                    const startType = formdata.startType;
+
+                    const aeData = {
+                        label: "New Effect",
+                        icon: "icons/svg/aura.svg",
+                        origin: owner.uuid
+                    };
+                    if (startType === 'nowGameTime') {
+                        aeData['duration.startTime'] = dialogData.gameTime;
+                        aeData['duration.seconds'] = 1;
+                    } else if (startType === 'nowCombat') {
+                        aeData['duration.combat'] = dialogData.combatId;
+                        aeData['duration.startRound'] = dialogData.combatRound;
+                        aeData['duration.startTurn'] = dialogData.combatTurn;
+                        aeData['duration.rounds'] = 1;
+                        aeData['duration.turns'] = 0;
+                    }
+
+                    console.log(aeData);
+                    return ActiveEffect.create(aeData, owner).create().then(ae => {
+                        console.log(ae);
+                        const e = owner.effects.get(ae._id);
+                        return e.sheet.render(true);
+                    })
+                },
+                options: { jQuery: false }
+            });
         case "edit":
-            const sh = effect.sheet;
-            console.log(effect);
             return effect.sheet.render(true);
         case "delete":
             return effect.delete();
