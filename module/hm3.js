47b659716982ba3b66034de9f0a6b3870ca584f0
Stephen Cooper <scooper4711@users.noreply.github.com>
2021-08-11 18:09:20 -0400

    Added color pickers to TinyMCE

--- module/hm3.js
+++ module/hm3.js
@@ -98,7 +98,7 @@ Hooks.once('init', async function () {
     });
 
     // Add a font selector dropdown to the TineMCE editor
-    CONFIG.TinyMCE.toolbar = "styleselect bullist numlist image table hr link removeformat code fontselect fontsizeselect save";
+    CONFIG.TinyMCE.toolbar = "styleselect forecolor backcolor bullist numlist image table hr link removeformat code fontselect fontsizeselect save";
     // Register the Hârnic fonts with Foundry and TinyMCE
     // These are the default fonts for browsers
     let defaultFonts = "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Signika=Signika,sans-serif;Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats"
